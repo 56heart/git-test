b8099374beb19cb1163aef1d9442ccaf184a0924
wh <1098046976@qq.com>
2022-11-11 15:38:58 +0800

    first commit

---
+++ src/main/java/wh/gitTest.java
@@ -0,0 +1,12 @@
+package wh;
+
+/**
+ * @author 大只
+ * @version 1.0
+ * @date 2022/11/11 15:18
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
